3a98d8d9de06c0d0d55dc5ab3614147e18bd58ed
david fils <miharisoadavidfils@gmail.com>
2025-09-03 09:23:10 +0300

    [ADD] : answer of 1.b

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 app = FastAPI()
+
+class Characteristic(BaseModel):
+    ram_memory: int
+    rom_memory: int
+
+class Phone(BaseModel):
+    identifier: str
+    brand: str
+    model: str
+    characteristics: Characteristic
+
+phones_store: List[Phone] = []
 
 @app.get("/health")
 def health():
     return "Ok"
+
+@app.post("/phones", status_code=201)
+def create_phones(new_phones: List[Phone]):
+    phones_store.extend(new_phones)
+    return [phone.model_dump() for phone in phones_store]
